0ce8ceab4c19a73c0197e83e7d01103ef9c1524c
Mark David Prado <155267500+kiritoS40@users.noreply.github.com>
2024-01-27 17:29:11 +0800

    Add GameFrame.java as the simulation app window container

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import View.GameFrame;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new GameFrame();
     }
 }
--- src/View/GameFrame.java
+++ src/View/GameFrame.java
@@ -1,4 +1,39 @@
 package View;
 
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class GameFrame {
+    public GameFrame() {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("TooFastForFood");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startAppThread();
+
+        // Add a WindowListener to prompt the user before closing
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int choice = JOptionPane.showConfirmDialog(window,
+                        "Are you sure you want to exit?",
+                        "Confirm Exit", JOptionPane.YES_NO_OPTION);
+
+                if (choice == JOptionPane.YES_OPTION) {
+                    // If the user chooses yes, close the application
+                    System.exit(0);
+                }
+            }
+        });
+    }
 }
